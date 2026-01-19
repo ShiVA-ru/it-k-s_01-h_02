d7890870bd02d3b4267bbc71719c9d408b315879
Shinyaev Vladimir <secman@bk.ru>
2026-01-19 17:36:45 +0500

    fix index for Vercel

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ setupApp(app);
 // порт приложения
 const PORT = process.env.PORT || 5001;
 
-// if (process.env.VERCEL !== "1") {
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-// }
+if (process.env.VERCEL !== "1") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 // Export for Vercel serverless functions
-// export default app;
+export default app;
